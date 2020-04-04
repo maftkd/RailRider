42ae928873349c6220f2f4c911c38cbee98a8bb5
Michael <michael.feldman09@gmail.com>
2020-04-04 13:44:49 -0400

    Added RailRider Unity project

---
+++ RailRiderUnity/Assets/_Scripts/Editor/BuildHelper.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class BuildHelper : EditorWindow
+{
+    private bool _includeBundles;
+    public enum BuildTargets { WINDOWS, LINUX, MAC, HTML };
+    public BuildTargets _curTarget;
+    public bool _deployBuilds;
+
+    public string _serverSCP;
+    public bool _loadCheck = false;
+
+    // Start is called before the first frame update
+    [MenuItem("BuildScript/Buildhtml")]
+    static void Init()
+    {
+        BuildHelper window = (BuildHelper)EditorWindow.GetWindow(typeof(BuildHelper));
+        window.Show();
+    }
+
+    private void OnGUI()
+    {
+        if(GUILayout.Button("Build And Push"))
+        {
+            BuildPlayer(BuildTargets.HTML);
+            //PushToItch();
+        }
+    }
+
+    private void BuildPlayer(BuildTargets target)
+    {
+        //add scenes to build list
+        string[] scenes = new string[EditorBuildSettings.scenes.Length];
+        for (int i = 0; i < scenes.Length; i++)
+        {
+            scenes[i] = EditorBuildSettings.scenes[i].path;
+            Debug.Log("Adding Scene: " + scenes[i]);
+        }
+
+        BuildTarget bTarget;
+        Debug.Log("Preparing build for target: " + target.ToString());
+        string buildPath = Directory.GetParent(Directory.GetParent(Application.dataPath).FullName).FullName.Replace('\\','/')+"/buildPath";
+        string extension = ".exe";
+        //determine platform specifics
+        switch (target)
+        {
+            case BuildTargets.WINDOWS:
+                buildPath += "/StandaloneWindows/";
+                bTarget = BuildTarget.StandaloneWindows;
+                break;
+            case BuildTargets.LINUX:
+                buildPath += "/StandaloneLinux64/";
+                bTarget = BuildTarget.StandaloneLinux64;
+                extension = ".x64";
+                break;
+            case BuildTargets.MAC:
+                buildPath += "/StandaloneOSX/";
+                bTarget = BuildTarget.StandaloneOSX;
+                break;
+            case BuildTargets.HTML:
+                buildPath += "/HTML/";
+                bTarget = BuildTarget.WebGL;
+                break;
+            default:
+                Debug.LogError("Unidentified / unsupported build platform: " + target.ToString());
+                return;            
+        }
+        if (!Directory.Exists(buildPath))
+            Directory.CreateDirectory(buildPath);
+
+        Debug.Log("Building player");
+        BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, buildPath , bTarget, BuildOptions.None);
+    }
+
+    private void PushToItch()
+    {
+        string bat = "set serverPath=" + _serverSCP + "\n";
+
+        bat += "cd %1\ndel *.zip\n";
+        bat += "7z a -spe StandaloneWindows.zip .\\StandaloneWindows\\*\n";
+        bat += "7z a -spe StandaloneOSX.zip .\\StandaloneOSX\\*\n";
+        bat += "7z a -spe StandaloneLinux64.zip .\\StandaloneLinux64\\*\n";
+        bat += "scp -rp *.zip %serverPath%\nexit";
+
+        string batPath = Application.dataPath + "/ABK/Scripts/PushBuilds.bat";
+        File.WriteAllText(batPath, bat);
+
+        string buildPath = Directory.GetParent(Directory.GetParent(Application.dataPath).FullName).FullName.Replace('\\', '/') + "/buildPath";
+        System.Diagnostics.Process.Start("cmd.exe", "/k " + batPath + " " + buildPath);
+    }
+
+
+    [System.Serializable]
+    public class BuildPathInfo
+    {
+        public string _windowsPath;
+        public string _macPath;
+        public string _linuxPath;
+    }
+}
---
+++ RailRiderUnity/Assets/_Scripts/Tubify.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Tubify : MonoBehaviour
+{
+    public MeshFilter _filter;
+    public Transform _points;
+    public int _numDivs;
+    public float _radius;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Mesh mesh;
+        if (_filter.sharedMesh != null)
+            mesh = _filter.sharedMesh;
+        else
+            mesh = new Mesh();
+        Vector3[] verts;
+        Vector3[] norms;        
+        Vector2[] uvs;
+        int[] tris;
+
+        tris = new int[6 * _numDivs * (_points.childCount - 1)];
+
+        verts = new Vector3[_points.childCount * _numDivs];
+        norms = new Vector3[_points.childCount * _numDivs];
+        uvs = new Vector2[_points.childCount * _numDivs];
+
+        for(int ring = 0; ring < _points.childCount; ring++)
+        {
+            float ringFrac = (float)ring / _points.childCount;
+            for (int div = 0; div < _numDivs; div++)
+            {
+                int index = ring * _numDivs + div;
+                Transform curPoint = _points.GetChild(ring);
+                float frac = ((float)div / _numDivs);
+                float angle = Mathf.PI * 2f * frac;
+                Vector3 offset = Vector3.zero;
+                offset += curPoint.right * Mathf.Cos(angle) * _radius + curPoint.up * Mathf.Sin(angle) * _radius;
+                verts[index] = curPoint.localPosition + offset;
+                norms[index] = offset;
+                uvs[index] = new Vector2(frac, ringFrac);
+
+                //triangle
+                if (ring < _points.childCount - 1)
+                {
+                    if(div < _numDivs - 1)
+                    {
+                        //use next div to complete tris
+                        tris[index * 6] = index;
+                        tris[index * 6 + 2] = index + _numDivs;
+                        tris[index * 6 + 1] = index + _numDivs + 1;
+                        tris[index * 6 + 3] = index;
+                        tris[index * 6 + 5] = index + _numDivs + 1;
+                        tris[index * 6 + 4] = index + 1;
+                    }
+                    else
+                    {
+                        //use first div to complete tris
+                        tris[index * 6] = index;
+                        tris[index * 6 + 2] = index + _numDivs;
+                        tris[index * 6 + 1] = index + 1;
+                        tris[index * 6 + 3] = index;
+                        tris[index * 6 + 5] = index + 1;
+                        tris[index * 6 + 4] = index + 1 - _numDivs;
+                    }
+                }
+            }
+        }
+
+        mesh.vertices = verts;
+        mesh.triangles = tris;
+        mesh.uv = uvs;
+        _filter.mesh = mesh;
+    }
+}
