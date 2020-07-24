a339f9c22bc6de1deba28fdb10a59cb7546a5ce4
Michael Feldman <michael.feldman09@gmail.com>
2020-07-24 11:01:20 -0400

    Rail continuously generates at hardcoded track value of 8. Only ever generates a single straight at a time. Old track and old coins get destroyed. Added lots of comments

--- RailRiderUnity/Assets/_Scripts/RailGenerator.cs
+++ RailRiderUnity/Assets/_Scripts/RailGenerator.cs
@@ -33,6 +33,7 @@ public class RailGenerator : MonoBehaviour
 	float _coinProbability = 0.3f;
 	int _minCoinCluster=3;
 	int _maxCoinCluster=8;
+	int _tOffset=0;
 
 	struct Coin {
 		public Transform transform;
@@ -54,99 +55,26 @@ public class RailGenerator : MonoBehaviour
 		//configure railTracker
 		_railTracker=transform.GetChild(0);
 
-		//Start out with a straight segment
-		_knots.Add(Vector3.zero);
-		_knots.Add(Vector3.forward*_nodeDist);
-		//then add some more...
-		AddStraight(2);
-		//add some curves and zig zag
-		AddCurve(_nodeDist*2,3,false);
-		AddZigZag(Mathf.PI/8f,4,false);
-		AddCurve(_nodeDist*1.5f,3,true);
-		AddStraight(2);
-		AddCurve(_nodeDist*1.5f,4,false);
-		AddCurve(_nodeDist*2.5f,8,true);
-		AddStraight(2);
-		AddZigZag(Mathf.PI/6f,8,false);
-		AddCurve(_nodeDist*3f,12,true);
+		GenerateStartingSection();
 		
-		//instantiate our cubic bezier path
-		_path = new CubicBezierPath(_knots.ToArray());	
-		
-		//set the rail pos count
-		_line.positionCount=_lineResolution*(_path.GetNumCurveSegments());
-		int clusterCounter=0;
-		float prevCross=0;
-		for(int i=0; i<_line.positionCount; i++){
-			//set the rail position at point t along rail
-			float t = i/(float)_lineResolution;
-			Vector3 railPos = _path.GetPoint(t);
-			Vector3 curForward = _path.GetTangent(t);
-			_line.SetPosition(i,railPos);
+		ResetRail();
 
-			if(i<_line.positionCount-1){
-
-				//if we are adding a cluster of coins
-				if(clusterCounter>0){
-
-					Coin c;
-					//determine the curvature
-					Vector3 nextForward = _path.GetTangent(t+1f/(float)_lineResolution);
-					float cross = Vector3.Cross(curForward,nextForward).y*.1f;
-					//make sure the delta isn't nuts
-					if(prevCross!=0 && Mathf.Abs(cross-prevCross)>0.1f)
-					{
-						clusterCounter=0;
-					}
-					else{
-						//rail curvature is smooth enough for a coin
-						Vector3 right = Vector3.Cross(Vector3.up,curForward);
-						Vector3 offset = Vector3.LerpUnclamped(Vector3.up,right,cross);
-						offset.Normalize();
-						//instance the coin
-						Transform curCoin = Instantiate(_coin,railPos+offset,Quaternion.identity, null);
-						c.transform = curCoin;
-						//set the line data
-						LineRenderer curLine = curCoin.GetComponent<LineRenderer>();
-						curLine.SetPosition(0,railPos);
-						curLine.SetPosition(1,railPos+offset*.3f);
-
-						c.line=curLine;
-						//get the coin mesh data
-						c.mesh = curCoin.GetComponent<MeshRenderer>();
-						//add coin to coin dict
-						_coins.Add(t,c);
-						clusterCounter--;
-						prevCross=cross;
-					}
-				}
-				else{
-					if(Random.value<_coinProbability)
-					{
-						Vector3 nextForward = _path.GetTangent(t+1f/(float)_lineResolution);
-						float cross = Vector3.Cross(curForward,nextForward).y*.1f;
-						if(cross>_crossThreshold)
-						{
-							clusterCounter=Random.Range(_minCoinCluster,_maxCoinCluster+1);
-							prevCross=0;
-						}
-					}
-				}
-			}
-		}
+		GenerateCoins(0,_knots.Count-1);
 
 		//Get some references
 		_helmet = GameObject.FindGameObjectWithTag("helmet").transform;
 		_followTarget = Camera.main.transform.GetComponent<FollowTarget>();
 
 		//start test
-		StartCoroutine(TestRide());
+		StartCoroutine(Ride());
 	}
 	
-	IEnumerator TestRide(){
+	IEnumerator Ride(){
 		float t=0;
 		float balance=0;
-		while(t<_knots.Count-1){
+		//this might need to get changed for an infinite track
+		//more of a state based riding condition
+		while(t-_tOffset<_knots.Count-1){
 			//get input
 			balance+=Input.GetAxis("Horizontal")*Time.deltaTime*_balanceSpeed;
 			if(Input.GetMouseButton(0))
@@ -181,13 +109,16 @@ public class RailGenerator : MonoBehaviour
 					_balanceVelocity = Mathf.Lerp(_balanceVelocity,-1f,_balanceAcceleration*Time.deltaTime);
 					break;
 			}
+
+			//set player's root position and orientation
 			balance-=_balanceVelocity*Time.deltaTime*_balanceSpeed;
 			Vector3 prevForward = _railTracker.forward;
-			//set player's root position and orientation
-			_railTracker.position = _path.GetPoint(t);
-			_railTracker.forward = _path.GetTangent(t);
+			float railT = t-_tOffset;
+			_railTracker.position = _path.GetPoint(railT);
+			_railTracker.forward = _path.GetTangent(railT);
 			Vector3 localEuler = _railTracker.localEulerAngles;
-			//lets try some "physics here"
+
+			//physics
 			float grav = Mathf.Abs(balance);
 			grav = grav<_gravityThreshold ? 0 : Mathf.InverseLerp(0,90,grav)*Mathf.Sign(balance)*_gravityPower;
 			float momentum = Vector3.Cross(prevForward, _railTracker.forward).y*_momentumPower;
@@ -195,7 +126,6 @@ public class RailGenerator : MonoBehaviour
 			balance+=grav;
 			balance-=momentum;
 			localEuler.z = -balance;
-			//set player's apparent balance on the rail
 			_railTracker.localEulerAngles=localEuler;
 
 			//Check for point acquisitions
@@ -203,9 +133,13 @@ public class RailGenerator : MonoBehaviour
 				if(f>t && f <t+2){
 					Coin c = _coins[f];
 					float sqrMag = (c.transform.position-_helmet.position).sqrMagnitude;
+					//if it's not in the ballpark just skip
 					if(sqrMag<400){
+						//this is not really ideal, we don't want to be setting this every frame they are close-by... I think...
 						c.mesh.enabled=true;
 						if(sqrMag>4){
+
+							//animate the indicator line
 							c.line.enabled=true;
 							LineRenderer r = c.line;
 							Vector3 pos0 = r.GetPosition(0);
@@ -215,19 +149,163 @@ public class RailGenerator : MonoBehaviour
 							pos1 = pos0+dif*_indicatorCurve.Evaluate(normDist)*_indicatorHeight;
 							r.SetPosition(1,pos1);
 							r.widthMultiplier=(1-normDist)*_indicatorWidth;
+							//animate the coin along with line
 							c.transform.position=pos1;
 						}
 						else
 							c.line.enabled=false;
 					}
+
+					//this hit detection logic is continuous and not really valid (frame rate dependent)
+					//temp code - need something more discrete
 					if(sqrMag<.3f)
 						Debug.Log("hit a coin");
 				}
 			}
 
+			//check for new track gen
+			if(t-_tOffset>_knots.Count-8){
+				//temp code - assumes adding a straight
+				//remove first knot
+				_knots.RemoveAt(0);
+
+				//might be a good time to clear out some of those dictionary items from the coins dict
+				//again temp code - assuming a single track was removed
+				ClearOldCoins(_tOffset+1);
+				//add a straight
+				AddStraight(1);
+				//reset cubic bezier path
+				//reset line renderer
+				ResetRail();
+				//increment tOffset
+				//Note: this corresponds to the number of knots REMOVED
+				_tOffset++;
+				//generate coins
+				GenerateCoins(_knots.Count-2,_knots.Count-1);
+			}
+
 			//tick for next frame
 			t+=Time.deltaTime*_moveSpeed;
 			yield return null;
+		}
+	}
+
+	void GenerateStartingSection(){
+		_knots.Add(Vector3.zero);
+		_knots.Add(Vector3.forward*_nodeDist);
+		//Starts out with 3 straights total
+		AddStraight(2);
+		//Then a long curve
+		AddCurve(_nodeDist*Random.Range(3,5f),6,(Random.value < 0.5f));
+	}
+
+	void GenerateCoins(int startKnot, int endKnot){
+		//coin generation
+		int clusterCounter=0;
+		float prevCross=0;
+		for(int i=startKnot*_lineResolution; i<endKnot*_lineResolution; i++){
+			//converts line space to coin space
+			float t = i/(float)_lineResolution;
+
+			//calculates the local position and tengent at t along rail
+			Vector3 railPos = _path.GetPoint(t);
+			Vector3 curForward = _path.GetTangent(t);
+
+			//validate rail index (don't add any past the end)
+			if(i<_line.positionCount-1){
+
+				//If there are still coins to be added in a cluster
+				if(clusterCounter>0){
+					//declare coin struct
+					Coin c;
+					//determine the curvature
+					Vector3 nextForward = _path.GetTangent(t+1f/(float)_lineResolution);
+					float cross = Vector3.Cross(curForward,nextForward).y*.1f;
+
+					//make sure the delta isn't nuts
+					if(prevCross!=0 && Mathf.Abs(cross-prevCross)>0.1f)
+					{
+						clusterCounter=0;
+					}
+					else{
+						//rail curvature is OK
+						//calculate the coins offset direction
+						Vector3 right = Vector3.Cross(Vector3.up,curForward);
+						Vector3 offset = Vector3.LerpUnclamped(Vector3.up,right,cross);
+						offset.Normalize();
+
+						//instance the coin
+						Transform curCoin = Instantiate(_coin,railPos+offset,Quaternion.identity, null);
+						c.transform = curCoin;
+
+						//set the line data
+						LineRenderer curLine = curCoin.GetComponent<LineRenderer>();
+						curLine.SetPosition(0,railPos);
+						//offset by .3 so the direction is preserved, but the magnitude will be controlled in the Ride CoRoutine
+						curLine.SetPosition(1,railPos+offset*.3f);
+						c.line=curLine;
+
+						//get the coin mesh data
+						c.mesh = curCoin.GetComponent<MeshRenderer>();
+
+						//add coin to coin dict
+						_coins.Add(t+_tOffset,c);
+						clusterCounter--;
+						prevCross=cross;
+					}
+				}
+				//If we are not currently generating a cluster
+				else{
+					//see if we randomly create a cluster
+					if(Random.value<_coinProbability)
+					{
+						//Make sure we don't generate coins on straight sections because that is boring and the coins are harder to see
+						Vector3 nextForward = _path.GetTangent(t+1f/(float)_lineResolution);
+						float cross = Vector3.Cross(curForward,nextForward).y*.1f;
+						if(Mathf.Abs(cross)>_crossThreshold)
+						{
+							//Determine cluster size
+							clusterCounter=Random.Range(_minCoinCluster,_maxCoinCluster+1);
+							prevCross=0;
+						}
+					}
+				}
+			}
+		}
+
+	}
+
+	void ClearOldCoins(int endClear){
+
+		//Determine which coins need to be removed
+		List<float> deleteKeys = new List<float>();
+		foreach(float f in _coins.Keys){
+			if(f<endClear)
+				deleteKeys.Add(f);
+			else
+				break;
+		}
+		
+		//Destroy coins and remove from dict
+		foreach(float f in deleteKeys){
+			Transform t = _coins[f].transform;
+			_coins.Remove(f);
+			Destroy(t.gameObject,Random.value);//stagger the recycling process over a second
+		}
+	}
+
+	void ResetRail(){
+		//instantiate our cubic bezier path
+		_path = new CubicBezierPath(_knots.ToArray());	
+		
+		//Rail (line renderer) setup
+		_line.positionCount=_lineResolution*(_path.GetNumCurveSegments());
+		for(int i=0; i<_line.positionCount; i++){
+			//set the rail position at point t along rail
+			float t = i/(float)_lineResolution;
+			Vector3 railPos = _path.GetPoint(t);
+			Vector3 curForward = _path.GetTangent(t);
+			_line.SetPosition(i,railPos);
 		}
 	}
 
