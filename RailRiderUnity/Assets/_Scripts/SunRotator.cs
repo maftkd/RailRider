9d4eac823514d65ae4fe92cd85f52fc757aa3bc7
Michael Feldman <michael.feldman09@gmail.com>
2020-07-29 13:46:56 -0400

    Added Tutorial

---
+++ RailRiderUnity/Assets/_Scripts/SunRotator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunRotator : MonoBehaviour
+{
+	[HideInInspector]
+	public bool _rotating=false;
+	public float _spinSpeed;
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if(_rotating)
+			transform.Rotate(_spinSpeed*Time.deltaTime,0,0);
+	}
+}
