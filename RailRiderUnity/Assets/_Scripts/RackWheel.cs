f8074cea747d217cb853fd773013f2adc003f251
Michael Feldman <michael.feldman09@gmail.com>
2021-07-21 09:21:48 -0400

    Physics testing and tweaking

--- RailRiderUnity/Assets/_Scripts/RackWheel.cs
+++ RailRiderUnity/Assets/_Scripts/RackWheel.cs
@@ -8,7 +8,7 @@ public class RackWheel : MonoBehaviour
 	Transform _pinion;
 	Transform _gear;
 	float _ratio;
-	float _hitThresh = 0.6f;
+	float _hitThresh = 0.65f;
     // Start is called before the first frame update
     void Start()
     {
